8295278125d9dc78cfa7d6eaec03f95abfc0a8d7
Satoshi <zeraming071@gmail.com>
2023-11-27 16:54:39 +0800

    Tempobranch affv2 (#2)
    * added Mediaquery and status
    
    * bug fix about the hour minutes and sec
    
    * updated
    
    * change folder
    
    * Nice
    
    * Create README.md
    
    aa
    
    * will add more fix later
    
    * Optimized code readability
    
    * added validation for insertion of data
    
    * update
    
    * minor fix
    
    * added fix
    
    * Add files via upload
    
    * added function on status
    
    * added Arduino code

---
+++ AFFv2incompletv3/AFFv2incompletv3.ino
@@ -0,0 +1,187 @@
+
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Servo.h>
+
+Servo servo;
+
+#define FIREBASE_HOST "frenzy-ecfc7-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "NvEgms7TtDDn0cliU7HsbktRPngvZd8yBFglhj6x"
+#define WIFI_SSID "5"
+#define WIFI_PASSWORD "150169226"
+
+
+
+
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP, "asia.pool.ntp.org",28800);
+
+const int trigPin = D1;  //D4
+const int echoPin = D2;  //D3
+
+long duration;
+int distance;
+
+FirebaseData timer,feed, fbdo;
+String timerPath;
+String schedule;
+String spin;
+String schedules[]={"00:00","00:00","00:00"} ;
+int spins[]{0,0,0};
+String currentTime;
+int i,feednow=0;
+
+
+
+
+void setup() 
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to a");
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());   
+  timeClient.begin();
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  servo.attach(D3, 500, 2400); // Pin attached to D3oi3579
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  
+}
+
+void loop() {
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  timeClient.update();
+
+  int hours = timeClient.getHours();
+  int minutes = timeClient.getMinutes();
+  //get time and hours`
+  currentTime = (hours < 10 ? "0" : "") + String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes);
+  
+  Firebase.getInt(feed,"feednow");
+  feednow = feed.to<int>();
+  ultrasonic();
+
+  if (feednow==1) // Direct Feeding
+  {
+      servo.write(0); // rotate clockwise
+      delay(900); // allow to rotate for n micro seconds, you can change this to your need
+      servo.write(180); // stop rotation
+      feednow = 0;
+      Firebase.setInt(feed, "/feednow",feednow);
+      Serial.println("Fed");
+      delay(900); 
+      servo.write(0); // stop rotation
+
+      recentfeed();
+  }
+   
+else
+ { // Scheduling feed
+    if(Firebase.get(fbdo,"/AFFV2"))
+    {
+      // check if there is the diredtory path in the firebase.
+      if(fbdo.dataType()=="null")
+      {
+        Serial.println("Database does not Exist:");
+      }
+      else
+      {  
+        Serial.println("Database does Exist:");
+        for (int i = 0; i < 3; i++) 
+        {
+          //time update in reaktime comminication
+          timeClient.update();
+          currentTime = (hours < 10 ? "0" : "") + String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes);
+          timerPath = "/AFFV2/timers/timer" + String(i);   
+
+          // Retrieve schedule value
+          Firebase.getString(fbdo, timerPath + "/schedule");
+          schedule = fbdo.to<String>();
+          schedules[i] = schedule;  
+         
+          // Retrieve spin value
+          Firebase.getInt(fbdo, timerPath + "/spin");
+          spin = fbdo.to<String>();
+          spins[i] = spin.toInt();
+
+          if(schedules[i] == currentTime)
+            {
+            int spininput=0;
+            
+            Serial.println("Schedule:" +schedules[i] );
+            spininput=i;
+              for(int j = 0; j< spins[spininput] ; j++)
+              {
+                servo.write(0); // rotate clockwise
+                delay(900); // allow to rotate for n micro seconds, you can change this to your need
+                servo.write(180); // stop rotation
+                Serial.println("Feeding" + j);
+                delay(900);      
+              }
+            Serial.println("Delay1 minute");
+            //delay 1 minute
+            recentfeed();
+            delay(60000);     
+            }  
+        }
+      } 
+    fbdo.clear();  
+    // Removes the the object inside fbdo
+    }
+    timeClient.update();
+       //prints the time before looping again
+ }
+
+}
+
+
+void ultrasonic(){
+FirebaseData firebaseDataUltrasonic;
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+
+Firebase.setInt(firebaseDataUltrasonic, "/distance", distance);
+}
+
+
+
+void recentfeed(){
+timeClient.update();
+int hours = timeClient.getHours();
+int minutes = timeClient.getMinutes();
+FirebaseData firebaserecentfeed;
+currentTime = (hours < 10 ? "0" : "") + String(hours) + ":" + (minutes < 10 ? "0" : "") + String(minutes);
+Serial.println("RecentFeed : " + currentTime);
+Firebase.setString(firebaserecentfeed, "/recentfeed", currentTime);
+}
+
+
---
+++ firebase.js
@@ -0,0 +1,262 @@
+
+// THIS IS THE CONFIG OF OUR FIREBASE REAL TIME DATABASE
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0WToORO9UG83T1MlsudbVripZ7FdGli0",
+    authDomain: "frenzy-ecfc7.firebaseapp.com",
+    databaseURL: "https://frenzy-ecfc7-default-rtdb.firebaseio.com",
+    projectId: "frenzy-ecfc7",
+    storageBucket: "frenzy-ecfc7.appspot.com",
+    messagingSenderId: "783829962407",
+    appId: "1:783829962407:web:35b6506ef000dd682fd582"
+  };
+
+// INITIALIZE FIREBASE
+firebase.initializeApp(firebaseConfig);
+  
+let timerPath, numberOfChildrenAsInt;
+let timersCount = 0;
+
+readDataFromFirebase();
+
+ 
+// SCHEDULE FEEDING
+document.querySelector("#inputsave").onclick = function () {
+
+  let scheduletime, spintimes;
+
+  scheduletime = document.querySelector("#inputtime").value;
+  spintimes = document.querySelector("#inputspin").value;
+
+  // Check if either scheduletime or spintimes is empty
+  if (scheduletime.trim() === "" || spintimes.trim() === "") {
+    alert("Please enter both a valid schedule time and spin value (spin value should be 1-10).");
+    // You may choose to handle this case in a way that fits your application
+    return; // Stop further execution if one of the fields is empty
+  }
+
+  // Check if spintimes is a valid non-negative number
+  if (Number.isInteger(Number(spintimes))&& spintimes >= 1 && spintimes<=10) {
+    timerPath = `AFFV2/timers/timer${timersCount}`;
+    timersCount++;
+    firebase.database().ref(timerPath).set({
+      schedule: scheduletime,
+      spin: spintimes
+    });
+
+    alert("Data Inserted");
+    readDataFromFirebase();
+  } else {
+    alert("Please enter a valid spin value (1 to 10).");
+    // You may choose to handle this case in a way that fits your application
+  }
+};
+
+// MANUAL FEEDING FUNCTION
+document.querySelector("#feedbtn").onclick = function () {
+    firebase.database().ref().update({
+      feednow: 1 
+  });
+  alert("Thank You for The Food");
+};
+
+
+
+
+// READ DATA FROM FIREBASE
+// Function to read data from Realtime Database
+function readDataFromFirebase() {
+  let database = firebase.database();
+  let dataRef = database.ref('AFFV2/timers');
+
+  dataRef.once('value', function(snapshot) {
+    let data = snapshot.val();
+
+    if (data !== null && data !== undefined) {
+      
+      numberOfChildrenAsInt = Object.keys(data).length; // Get the number of children
+
+      timersCount = parseInt(numberOfChildrenAsInt, 10); // Now you can use numberOfChildrenAsInt as an integer
+
+      console.log("Current Child Node "+ timersCount);
+    
+    displayDataInHtml(data); // Call a function to update the HTML div with the data
+      
+    } else {
+      console.log("Data is empty");
+      timersCount = 0;
+    }
+ 
+  }, function(error) {
+    console.error("Error reading data: " + error.code);
+  });
+
+
+
+
+  var distancedata = firebase.database().ref('/distance');
+
+  distancedata.on('value', function(snapshot) {
+   var data = snapshot.val();
+   // Handle the data
+   var formatteddistance = "Remaining : " + data + "%"; // For example, you can add some text to the data
+
+    // Update the <p> element
+    document.getElementById('remainingfood').innerText = formatteddistance;
+});
+
+
+var recentfeeddata = firebase.database().ref('/recentfeed');
+
+recentfeeddata.on('value', function(snapshot) {
+   var recentfeedvalue = snapshot.val();
+   // Handle the data
+   var formattedfeedvalue = "Recent: " + recentfeedvalue + ""; // For example, you can add some text to the data
+
+    // Update the <p> element
+    document.getElementById('recentfeed').innerText = formattedfeedvalue;
+});
+
+
+}
+
+
+// DELETE FUNCTION
+function deleteDataFromFirebase() {
+  let database = firebase.database();
+  let dataRef = database.ref('AFFV2/timers');
+
+  // Use limitToLast(1) to get the last item
+  dataRef.orderByKey().limitToLast(1).once('value')
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        let lastTimerKey = childSnapshot.key;
+
+        let lastTimerRef = database.ref('AFFV2/timers/' + lastTimerKey);
+
+        lastTimerRef.remove()
+          .then(function() {
+            console.log("Last data removed successfully!");
+
+            const pathReference = database.ref("/AFFV2");
+              // Use the once method to check if the path exists
+            pathReference.once('value')
+  .           then((snapshot) => {
+              if (snapshot.exists()) 
+              {
+                console.log("Path exists");
+                console.log("Remaining Child Node "+ timersCount);
+              } else 
+              {
+                document.querySelector("#dataDisplay").innerHTML = "--------NULL---------";
+                  console.log("Path does not exist");
+                  timersCount = 0;
+              }
+             })
+          })
+          .catch(function(error) {
+            console.error("Error removing last data: " + error.message);
+          });
+      });
+    })
+    .catch(function(error) {
+      console.error("Error getting last data: " + error.message);
+    });
+}
+
+
+//PROMPT BEFORE DELETING
+function createDeleteHandler() {
+  return function() {
+
+    let userResponse = confirm("This will delete the last data entered. Proceed?");
+    
+    if (userResponse) {
+ 
+      deleteDataFromFirebase();
+    } else {
+
+    }
+  };
+}
+
+
+//CLEAR ALL FUNCTION
+function clearAllTimers() {
+  let database = firebase.database();
+  let timersRef = database.ref('AFFV2/timers');
+
+  timersRef.remove()
+    .then(function() {
+      console.log("All timers cleared successfully!");
+      document.querySelector("#dataDisplay").innerHTML = "--------NULL---------";
+      timersCount = 0;
+    })
+    .catch(function(error) {
+      console.error("Error clearing timers: " + error.message);
+    });
+}
+
+
+// CLEAR ALL PROMPT
+// Add an event listener to a button or trigger for clearing all timers
+document.querySelector("#clear").onclick = function () {
+  let userResponse = confirm("Are you sure you want to clear all timers?");
+  
+  if (userResponse) {
+    clearAllTimers();
+  } else {
+    // User canceled the operation
+  }
+};
+
+
+
+// USING CONSTRUCTOR TO CREATE NEW DIV
+// Define a constructor for your data nodes
+function TimerData(timerKey, schedule, spin) {
+  this.timerKey = timerKey;
+  this.schedule = schedule;
+  this.spin = spin;
+}
+
+// ADD DATA TO MY SCHEDULE DYNAMICALLY
+function displayDataInHtml(data) {
+  let dataDisplayDiv = document.getElementById('dataDisplay');
+
+  if (dataDisplayDiv && data) {
+
+    dataDisplayDiv.innerHTML = "";
+
+  
+    for (let timerKey in data) {
+      if (data.hasOwnProperty(timerKey)) {
+        let timerData = data[timerKey];
+
+   
+        let timerInstance = new TimerData(timerKey, timerData.schedule, timerData.spin);
+
+     
+        let timerDiv = document.createElement('div');
+        timerDiv.classList.add('timer');
+
+ 
+        let htmlContent = "<p>Timer: " + timerInstance.timerKey + "</p>";
+        htmlContent += "<p>Schedule: " + timerInstance.schedule + "</p>";
+
+        if (timerInstance.spin) {
+      
+          htmlContent += "<p>Spin : " + timerInstance.spin + "</p>";
+        }
+
+  
+        timerDiv.addEventListener('click', createDeleteHandler(timerInstance.timerKey));
+
+
+        timerDiv.innerHTML = htmlContent;
+
+        dataDisplayDiv.appendChild(timerDiv);
+      }
+    }
+  }
+}
